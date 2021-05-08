8d436f9cd64ef58ad302d1b373a94d23819f2a67
akabe2010 <lilokilo96@mail.ru>
2021-05-08 10:45:35 +0400

    folders updated

---
+++ app/views/commentScreen/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const commentScreen = () => {
+
+}
+export default commentScreen;
---
+++ app/views/searchScreen/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const searchScreen = () => {
+
+}
+export default searchScreen;
